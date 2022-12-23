f0eb9aa94c8cc6d71e6b6b6d08c8a5f6ff108034
Fedor Indutny <238531+indutny@users.noreply.github.com>
2022-12-23 11:29:56 -0800

    benchmarks: initial
    Fix: #2

---
+++ benchmarks/index.ts
@@ -0,0 +1,77 @@
+import { watch, memoize } from '../src';
+
+type BenchOptions = Readonly<{
+  name: string;
+  iterations?: number;
+  run(): number;
+}>;
+
+function bench({ name, iterations = 100000, run }: BenchOptions): void {
+  const start = process.hrtime.bigint();
+  let acc = 0;
+  for (let i = 0; i < iterations; i++) {
+    acc += run();
+  }
+  const duration = process.hrtime.bigint() - start;
+
+  if (acc === 0) {
+    throw new Error('No side-effect');
+  }
+
+  const durationInSeconds = Number(duration) / 1e9;
+  const ops = iterations / durationInSeconds;
+  console.log(`${name}: ${ops.toFixed(3)} operations/secons`);
+}
+
+bench({
+  name: 'watch+unwrap',
+  run() {
+    const { proxy, watcher } = watch({ a: { b: [10, 20, 30] }, c: {} });
+    const derived = watcher.unwrap({ num: proxy.a.b[2], c: proxy.c });
+    watcher.stop();
+
+    return derived.c ? derived.num ?? 0 : 0;
+  },
+});
+
+{
+  const input = { a: { b: [10, 20, 30] } };
+  const { proxy, watcher } = watch(input);
+  watcher.unwrap({ c: proxy.a.b[2] });
+  watcher.stop();
+
+  bench({
+    name: 'isChanged',
+    run() {
+      const notChanged = !watcher.isChanged(input, { a: { b: [30, 40, 30] } });
+
+      const changed = watcher.isChanged(input, { a: { b: [30, 40, 50] } });
+
+      return notChanged && changed ? 1 : 0;
+    },
+  });
+}
+
+{
+  type DeepInput = {
+    a: {
+      b: {
+        c: Array<number>;
+      };
+    };
+  };
+  const fn = memoize((first: DeepInput, second: DeepInput): number => {
+    return (first.a.b.c[1] ?? 0) + (second.a.b.c[0] ?? 0);
+  });
+
+  const input: DeepInput = { a: { b: { c: [10, 20, 30] } } };
+
+  bench({
+    name: 'memoize',
+    run() {
+      return fn(input, {
+        a: { b: { c: [30, 40, 50] } },
+      });
+    },
+  });
+}
